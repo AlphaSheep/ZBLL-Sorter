b1cacb5fe27a1bf659148d0d2f5be297e2dcb17a
Alphasheep <brendan@alphasheep.co.za>
2016-01-06 22:42:41 +0200

    Update Copyright information

--- src/utilities.py
+++ src/utilities.py
@@ -1,7 +1,7 @@
 '''
 Created on 06 Jan 2016
 
-    Copyright (c) 2014 Brendan Gray and Sylvermyst Technologies
+    Copyright (c) 2016 Brendan Gray and Sylvermyst Technologies
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
