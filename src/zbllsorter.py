a08b11c363cf84cf598a5888caf8c20d93054239
Alphasheep <brendan@alphasheep.co.za>
2018-01-02 08:10:46 +0200

    Only create directories if images need to be generated in them

--- src/zbllsorter.py
+++ src/zbllsorter.py
@@ -24,16 +24,16 @@ Created on 03 Mar 2015
     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
     THE SOFTWARE.
-    
+
 '''
 
 import os
 import json
 import pylab
 
-from imagegenerator import plot, save
 from casegenerator import getUniqueZBLLCases
 from htmlgenerator2 import generateHTML
+from imagegenerator import plot, save
 from utilities import getList, probFractionString
 
 from constants import *
@@ -46,29 +46,29 @@ from constants import *
 def sortCases(zblls):
     keys = list(zblls.keys())
     keys.sort()
-    
+
     sortedZBLLdict = {}
-    
-    i=0    
+
+    i=0
     for k in keys:
         [ocll, cpll, epll] = k.split(' ')
-        
+
         ocllName = ocllCaseNames[ocll]
         collName = ocllName+cpllCaseNames[cpll]
         zbllName = collName+'-'+epll
-        
+
         print(str(i)+'.\tFound new case: ', ocllName+' : '+collName+'\t', k,'---', (len(zblls[k])))
-        
-        
+
+
         if not ocllName in sortedZBLLdict.keys():
             sortedZBLLdict[ocllName] = {}
-            
+
         if not collName in sortedZBLLdict[ocllName].keys():
-            sortedZBLLdict[ocllName][collName] = []            
-        
-        sortedZBLLdict[ocllName][collName].append((zbllName, k))        
-        i+=1    
-        
+            sortedZBLLdict[ocllName][collName] = []
+
+        sortedZBLLdict[ocllName][collName].append((zbllName, k))
+        i+=1
+
     return sortedZBLLdict
 
 
@@ -86,16 +86,18 @@ def getImages(sortedZBLLdict, generateImages=True, generateSetImages=True):
             for zbll in sortedZBLLdict[ocll][coll]:
 
                 zbllName = zbll[0]
-                
-                print(str(i)+'.\tSetting image for ',zbllName)
+
+                # print(str(i)+'.\tSetting image for ',zbllName)
 
                 path = 'images/' + coll + '/'
-                if not os.access(savePath + path.split('/')[0], 0):
-                    os.mkdir(savePath + path.split('/')[0])
-                if not os.access(savePath + path, 0):
-                    os.mkdir(savePath + path)
+                if generateSetImages or generateImages:
+                    if not os.access(savePath + path.split('/')[0], 0):
+                        os.mkdir(savePath + path.split('/')[0])
+                if generateImages:
+                    if not os.access(savePath + path, 0):
+                        os.mkdir(savePath + path)
                 zbllfilename = path + zbllName + '.png'
-                
+
                 # Generate OCLL image
                 if isFirstOCLL:
                     ocllfilename = "images/" + ocll + '.png'
@@ -106,58 +108,55 @@ def getImages(sortedZBLLdict, generateImages=True, generateSetImages=True):
                     isFirstOCLL = False
 
                 # Generate COLL image
-                if isFirstCOLL:                    
+                if isFirstCOLL:
                     collfilename = "images/"+coll+'.png'
-                    if generateSetImages:         
+                    if generateSetImages:
                         plot(zbll[1], stage="COLL")
                         save(savePath+collfilename, size=collImageSize)
                     collImg[coll] = collfilename
                     isFirstCOLL = False
-    
+
                 # Generate ZBLL image
                 if generateImages:
                     plot(zbll[1])
                     save(savePath+zbllfilename, size=zbllImageSize)
-                
+
                 zbllImg[zbllName] = zbllfilename
                 i=i+1
-                
+
     return ocllImg, collImg, zbllImg
 
 
 def getProbablities(zblls):
-    
+
     keys = list(zblls.keys())
     keys.sort()
-    
     ocllProbs = {}
-    collProbs = {}    
-    zbllProbs = {}        
-    
+    collProbs = {}
+    zbllProbs = {}
+
     for k in keys:
         [ocll, cpll, epll] = k.split(' ')
-        
+
         ocllName = ocllCaseNames[ocll]
         collName = ocllName+cpllCaseNames[cpll]
         zbllName = collName+'-'+epll
-              
+
         if not ocllName in ocllProbs.keys():
             ocllProbs[ocllName] = len(zblls[k])
-        else: 
+        else:
             ocllProbs[ocllName] += len(zblls[k])
-            
+
         if not collName in collProbs.keys():
             collProbs[collName] = len(zblls[k])
-        else: 
+        else:
             collProbs[collName] += len(zblls[k])
-            
         if not zbllName in zbllProbs.keys():
             zbllProbs[zbllName] = len(zblls[k])
-        else: 
-            zbllProbs[zbllName] += len(zblls[k])            
-                
+        else:
+            zbllProbs[zbllName] += len(zblls[k])
+
     return ocllProbs, collProbs, zbllProbs
-    
 
 def dumpZbllJson(zbllDict):
     zbllNameDict = {}
@@ -175,30 +174,15 @@ def dumpZbllJson(zbllDict):
 #-------------------------
 
 def main():
-    
+
     zblls = getUniqueZBLLCases()
+    sortedZBLLdict = sortCases(zblls)
     
     print(' Number of unique cases found:',len(zblls))
-    
-    sortedZBLLdict = sortCases(zblls)
 
     ocllImg, collImg, zbllImg = getImages(sortedZBLLdict, generateImages=globalGenerateImages, generateSetImages=globalGenerateSetImages)    
     ocllProbs, collProbs, zbllProbs = getProbablities(zblls) 
 
-    
-    if False:
-        print('\n\n\n')
-        l = list(ocllProbs.keys())
-        l.sort()
-        for p in l:
-            print(p,round(ocllProbs[p]/77.76,2), probFractionString(ocllProbs[p],7776))
-        print('\n\n\n')
-        
-        l = list(collProbs.keys())
-        l.sort()
-        for p in l:
-            print(p,round(collProbs[p]/77.76,2), probFractionString(collProbs[p],7776))
-    
     
     html = generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collImg, zbllImg)
     
