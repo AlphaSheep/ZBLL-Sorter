1568b3786480de0917c5f3bde5e916387fa8e18c
Alphasheep <brendan@alphasheep.co.za>
2018-01-02 08:07:10 +0200

    Add summary of counts of known cases to the top of the web output

--- src/htmlgenerator2.py
+++ src/htmlgenerator2.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''
 Created on 06 Jan 2016
 
@@ -37,9 +38,24 @@ def generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collI
         background-color: #000;
         text-align: center;
     }
-            
+
+    div.zbllalgcount {
+        background: #222;
+        font-family: monospace;
+        color: #ddd;
+        padding: 15px;
+        margin: 20px 10px;
+        height: 64px;
+        text-align: left;
+        font-size: 1.2em;
+    }
+
+    div.zbllalgcount div {
+        padding: 5px ;
+    }
+
     div.zbllblock {
-    
+
         color: #ddd;
         background: #444;
         font-family: Verdana;
@@ -48,16 +64,15 @@ def generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collI
         display: block;
         text-align: center;
         border: 1px solid #000;
-        width: 105px;   
-        height: 190px;     
+        width: 105px;
+        height: 190px;
         padding: 2px;
     }
-    
     div.zbllalg {
         padding: 8px 2px;
         font-size: 8pt;
     }
-    
+
     div.zbllblock:hover, div.collblock:hover, div.ocllblock:hover {
         background: #446;
     }
@@ -73,12 +88,12 @@ def generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collI
     }
     """
 
-    cssFileName = 'stylesheet.css'
+    cssFileName = 'main.css'
+    cssPath = savePath + '/css/' + cssFileName
 
     copyrightMsg = """
     Copyright &copy; 2015 Brendan Gray and Sylvermyst Technologies
     """
-    
     i=0
     html = '<html>\n    <head>\n'
     html+= '        <link rel="stylesheet" type="text/css" href="css/'+cssFileName+'">\n'
@@ -86,41 +101,55 @@ def generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collI
     html+= '    </head>\n    <body>\n'
 
     x = 0
-    y = 60
-    
+    y = 150
+
     oclls = getList(sortedZBLLdict)
     algs = readKnownCases()
     
+    algcount = len(algs.keys())
+    casecount = len(zbllProbs.keys())-1
+
+    algcountminussune = len([k for k in algs.keys() if k[0] not in 'AS'])
+    casecountminussune = len([k for k in zbllProbs.keys() if k[0] not in 'AS'])-1
+
+    html += '        <div class="zbllalgcount"><div>Progress toward full ZBLL: '
+    html += '<b>{:} / {:}</b> ({:.2f}%)</div>\n'.format(algcount, casecount, algcount/casecount*100)
+    html += '            <div>Excluding Sune and Anti-Sune: '
+    html += '<b>{:} / {:}</b> ({:.2f}%)</div>\n'.format(algcountminussune, casecountminussune, algcountminussune/casecountminussune*100)
+    html += '        </div>'
+
+    html += '        <div class="zbllcontainer">\n\n'
+
     for ocll in oclls:
-        
+
         y += 2
-                
+
         colls = getList(sortedZBLLdict[ocll])
         for coll in colls:
-            
-            x= 80#165
-            
+
+            x = 20#165
+
             css+='\n#Case'+coll+' {\n    left: '+str(x)+';\n    top: '+str(y)+';\n}'
             x += 0#162
             y += 0
-            
+
             for zbll in sortedZBLLdict[ocll][coll]:
                 if zbll[0] in algs.keys():
                     classes = ' knownfull'
                 else:
                     classes = ' notknown'
-                
+
                 html += '    <div class="zbllblock'+classes+'" id="CaseZBLL'+str(i)+'">'
                 html += 'ZBLL case #'+str(i)+'<br/>'+zbll[0]+'\n        '
                 html += "<br/>P: "+"{:.2f}".format(zbllProbs[zbll[0]]/77.76)
                 html += "% ("+probFractionString(zbllProbs[zbll[0]], 7776)+")\n <br/><br/>        "
                 html += '<div class="cube-image" data-case="'+zbll[0]+'"></div>'
-                
+
                 if zbll[0] in algs.keys():
                     html+='<br/><div class="zbllalg">'+algs[zbll[0]]+'</div>\n'
                 else:
                     html+='\n'
-                    
+
                 html+='    </div>\n'
 
                 css+='\n#CaseZBLL'+str(i)+' {\n    left: '+str(x)+';\n    top: '+str(y)+';\n}'
@@ -129,10 +158,10 @@ def generateHTML(sortedZBLLdict, ocllProbs, collProbs, zbllProbs, ocllImg, collI
                 i+=1
 
             y += 201
-        
+
         y += 60
         x = 0
-        
+
     html+= '\n        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>\n'
     html+= '\n        <script src="js/cube.js"></script>\n'
     html+= '\n\n        </div>\n    </body>\n</html>\n'
